71d8a2086ae09010871fee3a05d7493e3db121bc
Deepesh <161422752+deepu4402020@users.noreply.github.com>
2026-01-03 12:27:30 +0530

    Time: 56 ms (54.77%) | Memory: 93 MB (49.93%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int t[100001][5][2];
+    int solve(vector<int>& prices, int i, int rem, bool flag) {
+        if (rem <= 0 || i >= prices.size()) {
+            return 0;
+        }
+        if (t[i][rem][flag] != -1) {  // Fixed: use t[i][rem][flag] for both check and store
+            return t[i][rem][flag];
+        }
+        // skip
+        int skip = solve(prices, i + 1, rem, flag);
+        int take = 0;
+
+        
+        // buy or sell
+        if (flag) {  // can buy
+            take = -prices[i] + solve(prices, i + 1, rem, 0);
+        } else {  // must sell
+            take = prices[i] + solve(prices, i + 1, rem - 1, 1);
+        }
+        return t[i][rem][flag] = max(skip, take);  
+    }
+
+    int maxProfit(vector<int>& prices) {
+        memset(t, -1, sizeof(t));
+        return solve(prices, 0, 2, 1); 
+    }
+};
